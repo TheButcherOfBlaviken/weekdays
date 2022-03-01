99365dbdb4f1f66f1d2b2d11b07beadf9c1b6a5d
TheButcherOfBlaviken <nabiladnan99@gmail.com>
2022-03-01 20:36:24 +0100

    user input added

--- weekdays.py
+++ weekdays.py
@@ -6,9 +6,26 @@ def daterange(date1, date2):
         yield date1 + timedelta(n)
 
 
-start_dt = date(2021, 12, 1)
-end_dt = date(2021, 12, 31)
-
+input_complete = False
+while not input_complete:
+    try:
+        start_year = int(input("Start Year: "))
+        start_month = int(input("Start Month: "))
+        start_date = int(input("Start Date: "))
+        end_year = int(input("end Year: "))
+        end_month = int(input("end Month: "))
+        end_date = int(input("end Date: "))
+    except ValueError:
+        print("\n~~~~~~~~~~~~~~~~~ERROR~~~~~~~~~~~~~~~~~")
+        print("Try a number.")
+        print("~~~~~~~~~~~~~~~~~ERROR~~~~~~~~~~~~~~~~~\n")
+        continue
+    else:
+        start_dt = date(start_year, start_month, start_date)
+        end_dt = date(end_year, end_month, end_date)
+        input_complete = True
+            
+            
 weekends = [6,7]
 for dt in daterange(start_dt, end_dt):
     if dt.isoweekday() not in weekends:
